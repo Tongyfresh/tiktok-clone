6fab10d03cec9ec6d0a222a9f086cbf6f03b2b36
tongyfresh <tip.bridge@gmail.com>
2025-01-30 09:42:42 -0700

    adjusted some of the db settings

--- backend/tiktokbackend/settings.py
+++ backend/tiktokbackend/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'tiktokbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tiktokdb',
         'USER': 'tiktokuser',
         'PASSWORD': 'tiktokpassword',
--- backend/videos/models.py
+++ backend/videos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    file = models.FileField(upload_to='videos/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
